7e3e84062623aac92c8808a29738210e39e5b1f7
luanf38 <luanfelipe38@hotmail.com>
2025-06-16 22:51:47 -0300

    Optimized imports

--- app/src/main/java/com/luanlima/musicplayer/MainActivity.kt
+++ app/src/main/java/com/luanlima/musicplayer/MainActivity.kt
@@ -6,25 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.tooling.preview.Preview
-import com.luanlima.musicplayer.ui.theme.MusicPlayerTheme
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.media3.exoplayer.ExoPlayer
+import com.luanlima.musicplayer.ui.theme.MusicPlayerTheme
 
 
 class MainActivity : ComponentActivity() {
