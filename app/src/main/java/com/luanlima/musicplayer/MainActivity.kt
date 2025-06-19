b6d1780f7891496f1c77aee3f48557711938cd09
luanf38 <luanfelipe38@hotmail.com>
2025-06-19 13:50:39 -0300

    Criado media item

--- app/src/main/java/com/luanlima/musicplayer/MainActivity.kt
+++ app/src/main/java/com/luanlima/musicplayer/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
+import androidx.media3.common.MediaItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,15 +26,23 @@ class MainActivity : ComponentActivity() {
 
         val player = ExoPlayer.Builder(this).build()
 
+        val mediaItem = MediaItem.fromUri("")
+
+        player.setMediaItem(mediaItem)
+
+        player.prepare()
+
         enableEdgeToEdge()
         setContent {
             MusicPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    Greeting(
-                        onClick = player::play,
-                        modifier = Modifier
-                            .padding(it)
-                    )
+                    Column {
+                        MusicPlayerApp(
+                            onClick = player::play,
+                            modifier = Modifier
+                                .padding(it)
+                        )
+                    }
                 }
             }
         }
@@ -41,7 +50,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(
+fun MusicPlayerApp(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -66,7 +75,7 @@ fun Greeting(
 fun GreetingPreview() {
     MusicPlayerTheme {
         Scaffold(Modifier.fillMaxSize()) {
-            Greeting(
+            MusicPlayerApp(
                 onClick = {},
                 modifier = Modifier.padding(it)
             )
